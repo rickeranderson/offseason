aa373e650919b383c4286dafde5725fdebd5eb1f
Rick Anderson <RickAnderson1@boisestate.edu>
2018-11-07 15:58:21 -0700

    updated text, changed pw

--- client/src/app/core/data/auth.ts
+++ client/src/app/core/data/auth.ts
@@ -1,8 +1,8 @@
 import { User } from '../models/user.model';
 
 export class AuthData {
-    private admin = 'adminPassword';
-    private player = 'userPassword';
+    private admin = 'adminRacers!';
+    private player = 'racers!';
 
     constructor() {
     }
